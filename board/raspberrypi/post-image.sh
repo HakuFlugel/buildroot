013c6d94ecbeae1cd1103d6c1e9961d6644b5e82
ShanoOrbi <ssa@orbital.dk>
2021-10-04 08:27:31 +0200

    raspberrypi confiiigs

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -7,6 +7,13 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+search="console=ttyAMA0,115200"
+replace=" "
+
+if [[ $search != "" && $replace != "" ]]; then
+sed -i "s/$search/$replace/" $filename
+fi
+
 for arg in "$@"
 do
 	case "${arg}" in
@@ -15,8 +22,15 @@ do
 			echo "Adding 'dtoverlay=miniuart-bt' to config.txt (fixes ttyAMA0 serial console)."
 			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 
-# fixes rpi (3B, 3B+, 3A+, 4B and Zero W) ttyAMA0 serial console
-dtoverlay=miniuart-bt,krnbt=on
+# fixes rpi (3B, 3B+, 3A+, 4B and Zero W) ttyAMA0 serial console ,krnbt=on
+dtoverlay=dwc2,dr_mode=host,disable-bt
+dtoverlay=uart0
+dtoverlay=uart-ctsrts
+#dtoverlay=uart2,ctsrts
+#dtoverlay=uart3,ctsrts
+dtoverlay=uart4,ctsrts
+#dtoverlay=uart5
+#dtoverlay=spi6-1cs
 __EOF__
 		fi
 		;;
@@ -49,7 +63,7 @@ trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
 
 rm -rf "${GENIMAGE_TMP}"
-
+cp "${BOARD_DIR}/uart-ctsrts.dtbo" "${BINARIES_DIR}/rpi-firmware/overlays/"
 genimage \
 	--rootpath "${ROOTPATH_TMP}"   \
 	--tmppath "${GENIMAGE_TMP}"    \
